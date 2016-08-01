702d43b6c0093fbc7cb1254299be2d8b7ee62347
tamasan810 <wataru.abe.5n@stu.hosei.ac.jp>
2016-08-01 11:34:04 +0900

    テスト

--- app/src/main/java/com/example/admin/googlemapkitkat/MapsActivity.java
+++ app/src/main/java/com/example/admin/googlemapkitkat/MapsActivity.java
@@ -43,6 +43,8 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.PolygonOptions;
 import com.google.android.gms.maps.model.PolylineOptions;
 
+// テストメッセージ
+
 // FragmentActivity is included in ActionBarActivity
 // FragmentActivity, AppCompatActivity
 
